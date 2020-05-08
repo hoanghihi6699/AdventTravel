f05f3c01b99ad0ee73610ab828b542310006f8cf
hoanghihi6699 <trananhhoang6699@gmail.com>
2020-05-08 10:41:31 +0700

    Commit2

--- js/custom.js
+++ js/custom.js
@@ -15,3 +15,45 @@ function srcollToTop() {
     document.body.scrollTop = 0; // For Safari
     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
+
+
+// Slide
+
+var _imgArray = [];
+var index = 0;
+var tagImg;
+var t;
+
+function slideShow() {
+    tagImg = document.getElementById('slide-show');
+    for (var i = 0; i < 4; i++) {
+        _imgArray[i] = new Image();
+        _imgArray[i].src = 'img/image/nha-trang/nha-trang-' + (i + 1) + '.jpg';
+        console.log(_imgArray[i].src);
+        index = i;
+    }
+    _autoPlay();
+}
+
+function _autoPlay() {
+    tagImg.style.backgroundImage = 'linear-gradient(rgba(136, 100, 33, 0.4),rgba(192, 192, 192, 0.4)),url(\'' + _imgArray[index].src + '\')';
+    index++;
+    t = setTimeout('_autoPlay()', 3800);
+    if (index == _imgArray.length) {
+        index = 0;
+    }
+}
+
+function next() {
+    console.log(index);
+    if (index < (_imgArray.length)) {
+
+        tagImg.style.backgroundImage = 'linear-gradient(rgba(136, 100, 33, 0.4),rgba(192, 192, 192, 0.4)),url(\'' + _imgArray[index].src + '\')';
+        console.log(tagImg.style.backgroundImage);
+        index++;
+    }
+    else {
+        tagImg.style.backgroundImage = 'linear-gradient(rgba(136, 100, 33, 0.4),rgba(192, 192, 192, 0.4)),url(' + _imgArray[0].src + ');';
+        index = 0;
+    }
+}
